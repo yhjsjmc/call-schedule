d7c5766d5b70bf99cb293f4a1c98dc0ba36c9115
yhjsjmc <younhoajung@gmail.com>
2025-11-05 12:57:40 -0500

    Update and rename heatmap to heatmap.py

---
+++ pages/heatmap.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime, timedelta
+import calendar
+import os
+
+st.set_page_config(page_title="Heatmap")
+st.title("Heatmap based on FTE")
+
+
